01aef542de041e8366f27333f25cc40d1516aff3
Lonny Kapelushnik <lonny@lonnylot.com>
2024-04-21 10:46:49 -0600

    Add parallel testing option

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,26 @@
 
 namespace Tests;
 
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\ParallelTesting;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    public function createApplication()
+    {
+        $app = parent::createApplication();
+
+        ParallelTesting::resolveOptionsUsing(function ($option) {
+            if ($option === 'connections') {
+                return [ 'mysql', 'pgsql' ];
+            }
+        
+            $option = 'LARAVEL_PARALLEL_TESTING_'.Str::upper($option);
+        
+            return $_SERVER[$option] ?? false;
+        });
+        
+        return $app;
+    }
 }
